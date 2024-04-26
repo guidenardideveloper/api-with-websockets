20360c2dd583718efadae4e41fceb78caffeac8f
Guilherme Denardi <gdenardi@outlook.com>
2024-04-26 10:48:44 -0300

    Adding Dynamic Data to the Payments Page

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def create_payment_pix():
                     "payment": new_payment.to_dict()})
 
 
-@app.route("/payments-pix-qr_code/<file_name>", methods=["GET"])
+@app.route("/payments/pix/qr_code/<file_name>", methods=["GET"])
 def get_image(file_name):
     return send_file(f"static/img/{file_name}.png", mimetype="image/png")
 
@@ -44,7 +44,9 @@ def pix_confirmation():
 
 @app.route("/payments/pix/<int:payment_id>", methods=["GET"])
 def payment_pix_page(payment_id):
-    return render_template("payment.html")
+    payment = Payment.query.get(payment_id)
+
+    return render_template("payment.html", payment_id=payment.id, value=payment.value, host="http://127.0.0.1:5000", qr_code=payment.qr_code)
 
 
 if __name__ == "__main__":
